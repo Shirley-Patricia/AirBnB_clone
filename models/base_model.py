405db61731103ec5e17af1115d995590e229af2d
Shirley <shircar16@gmail.com>
2021-11-09 13:58:07 -0500

    class BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,23 @@
 
 """
 
+from datetime import datetime
+
 class BaseModel:
 
-	def __init__(self, id, )
+	def __init__(self, id, created_at, updated_at):
+		self.id = id
+		self.created_at = datetime.now().isoformat()
+		self.updated_at = updated_at
+
+	def __str__(self):
+		"""should print [<class name>] (<self.id>) <self.__dict__>"""
+		return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
+
+	def save(self):
+		"""updates the public instance attribute updated_at with the current datetime"""
+		self.updated_at = datetime.now().isoformat()
+
+	def to_dict(self):
+
+
