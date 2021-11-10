d7818a4933b0c8f2e65f704e3f6ade9af1caf93e
Shirley <shircar16@gmail.com>
2021-11-09 23:34:06 -0500

    added to_dict

--- models/base_model.py
+++ models/base_model.py
@@ -4,22 +4,38 @@
 """
 
 from datetime import datetime
+from uuid import uuid4
+
 
 class BaseModel:
 
-	def __init__(self, id, created_at, updated_at):
-		self.id = id
-		self.created_at = datetime.now().isoformat()
-		self.updated_at = updated_at
+    def __init__(self, id, created_at, updated_at):
+        self.id = str(uuid4())
+        self.created_at = datetime.now().isoformat()
+        self.updated_at = datetime.now().isoformat()
 
-	def __str__(self):
-		"""should print [<class name>] (<self.id>) <self.__dict__>"""
-		return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
+    def __str__(self):
+        """should print [<class name>] (<self.id>) <self.__dict__>"""
 
-	def save(self):
-		"""updates the public instance attribute updated_at with the current datetime"""
-		self.updated_at = datetime.now().isoformat()
+        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
 
-	def to_dict(self):
+    def save(self):
+        """updates the public instance attribute updated_at
+        with the current datetime
+        """
 
+        self.updated_at = datetime.now().isoformat()
 
+    def to_dict(self):
+        """returns a dictionary containing all keys/values
+        of __dict__ of the instance"""
+
+        dict_BaseModel = {}
+        for key, value in self.__dict__.items():
+        dict_rectangle[key] = value
+        dict_BaseModel["__class__"] = self.__class__.__name__
+        dict_BaseModel["updated_at"] = self.updated_at
+        dict_BaseModel["id"] = self.id
+        dict_BaseModel["created_at"] = self.created_at
+
+        return dict_BaseModel
