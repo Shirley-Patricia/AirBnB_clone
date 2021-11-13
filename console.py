489937c0187e3a95cf8c1a39acc24667c0885278
Shirley <shircar16@gmail.com>
2021-11-13 15:42:30 -0500

    functions

--- console.py
+++ console.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 
 import cmd, sys
+from models import storage
+from models.base_model import BaseModel
+#from models import BaseModel
+
+lists = ["BaseModel"]
 
 class HBNBCommand(cmd.Cmd):
         intro = 'Welcome to the Python Console.   Type help or ? to list commands.\n'
@@ -11,10 +16,41 @@ class HBNBCommand(cmd.Cmd):
                 print("Hola soy un test")
 
         def do_quit(self, arg):
+                """Command to exit the program"""
                 return True
 
         def do_EOF(self, arg):
+                """Command to exit the program"""
                 return True
+        
+        def do_create(self, arg):
+                """
+                
+                """
+                if not arg:
+                        print("** class name missing **")
+                elif arg not in lists:
+                        print("** class doesn't exist **")
+                elif arg in lists:
+                        print("** class already exists **")
+                else:
+                        my_model = BaseModel()
+                        my_model.save()
+                        print(my_model.id)
+
+        def do_show(self,arg):
+                if not arg:
+                        print("** class name missing **")
+                elif arg not in lists:
+                        print("** class doesn't exist **")
+
+        def do_destroy(self, arg):
+                if not arg:
+                        print("** class name missing **")
+                elif arg not in lists:
+                        print("** class doesn't exist **")
+                else:
+                        del BaseModel.id
 
 
 if __name__ == '__main__':
