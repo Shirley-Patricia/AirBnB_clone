c21dd9a3d1c307a85444be7ed682baf92c9c770e
Shirley <shircar16@gmail.com>
2021-11-14 14:04:55 -0500

    add documentation

--- console.py
+++ console.py
@@ -37,7 +37,8 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, arg):
         """
-
+            Creates a new instance of BaseModel, saves it (to the JSON file)
+            and prints the id
         """
         if not arg:
             print("** class name missing **")
