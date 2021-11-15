61518cbe2bfc3f0af6c3f8666b5d02db4c6d17b7
ele4327 <sebasgarzons88@gmail.com>
2021-11-15 00:07:47 -0500

    Update Tests

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -2,15 +2,32 @@
 """Testing Amenity class"""
 import os
 import unittest
-import pycodestyle
-import models
-from models import amenity
+from models.amenity import Amenity
 from models.base_model import BaseModel
 
 
 class TestAmenity(unittest.TestCase):
-    """
-    """
+    """Tests class Amenity"""
+
+    def setUp(self):
+        """creating object"""
+        self.My_amenity = Amenity()
+
+    def test_type_amenity(self):
+        """tests type class Amenity"""
+        self.assertEqual(type(self.My_amenity), Amenity)
+
+    def test_attr_basemod(self):
+        """test attr inherites of BaseModel"""
+        self.assertTrue(hasattr(self.My_amenity, "id"))
+        self.assertTrue(hasattr(self.My_amenity, "created_at"))
+        self.assertTrue(hasattr(self.My_amenity, "updated_at"))
+        self.assertTrue(hasattr(self.My_amenity, "__class__"))
+
+    def test_attr_class(self):
+        """test attr of class"""
+        self.assertTrue(hasattr(self.My_amenity, "name"))
+        self.assertEqual(type(self.My_amenity.name), str)
 
 
 if __name__ == '__main__':
